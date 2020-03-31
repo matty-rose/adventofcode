f25040840d7f4195d9f380cf86d6f4445d4d308c
Matt Rose <matthewrose153@gmail.com>
2020-03-31 21:18:51 +1000

    Add data dir to helpers for easy import

--- python_2017/aoc/helpers.py
+++ python_2017/aoc/helpers.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+DATA_DIR = Path("/home/matt/Documents/AoC/python_2017/data")
+
 def parse_input(input_file_dir: str):
     with open(input_file_dir, 'r') as f:
         out = f.readlines()
