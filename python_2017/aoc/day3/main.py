55aabe42a57fdc57508d21ca4fb5e34da085776c
Matt Rose <matthewrose153@gmail.com>
2020-03-31 22:10:58 +1000

    day3 in progress

---
+++ python_2017/aoc/day3/main.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+INP = 289326
+
+"""
+0,0
+1,0
+1,1
+0,1
+-1,1
+-1,0
+-1,-1
+0,-1
+1,-1
+2,-1
+2,0
+2,1
+2,2
+1,2
+0,2
+-1,2
+-2,2
+-2,1
+-2,0
+-2,-1
+-2,-2
+"""
+
+def part_one(sq):
+    
+    pass
+
+def part_two():
+    pass
+
+if __name__ == "__main__":
+    print(part_one())
