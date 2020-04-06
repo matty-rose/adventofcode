11e285cee9bfa17c59f198dbfa8aa777a239c389
Matt Rose <matthewrose153@gmail.com>
2020-04-06 21:14:39 +1000

    day 8 and 9

---
+++ python_2017/aoc/day8/main.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+from aoc.helpers import parse_input, DATA_DIR
+
+dpath = DATA_DIR / "day8.txt"
+
+def proc_line(line):
+    l = line.strip()
+    split = l.split(" ")
+    ret = {
+        "register": split[0],
+        "op": split[1],
+        "val": int(split[2]),
+        "cond": " ".join(split[3:])
+    }
+    return ret
+
+def part_one(instructions):
+    registers = {r: 0 for r in list(set([ins["register"] for ins in instructions]))}
+    for ins in instructions:
+        reg = ins["register"]
+        split_cond = ins["cond"].split()
+        split_cond[1] = f"registers['{split_cond[1]}']"
+        altered_cond = " ".join(split_cond[1:])
+        if eval(altered_cond):
+            if ins["op"] == "inc":
+                registers[reg] = registers[reg] + ins["val"]
+            elif ins["op"] == "dec":
+                registers[reg] = registers[reg] - ins["val"]
+            else:
+                print("This operation isn't valid")
+                continue
+    max_val = max(registers.values())
+    return max_val
+
+def part_two(instructions):
+    registers = {r: 0 for r in list(set([ins["register"] for ins in instructions]))}
+    cur_max = 0
+    for ins in instructions:
+        reg = ins["register"]
+        split_cond = ins["cond"].split()
+        split_cond[1] = f"registers['{split_cond[1]}']"
+        altered_cond = " ".join(split_cond[1:])
+        if eval(altered_cond):
+            if ins["op"] == "inc":
+                registers[reg] = registers[reg] + ins["val"]
+                if registers[reg] > cur_max:
+                    cur_max = registers[reg]
+            elif ins["op"] == "dec":
+                registers[reg] = registers[reg] - ins["val"]
+                if registers[reg] > cur_max:
+                    cur_max = registers[reg]
+            else:
+                print("This operation isn't valid")
+                continue
+    return cur_max
+
+if __name__ == "__main__":
+    raw_inp = parse_input(dpath)
+    proc = [proc_line(l) for l in raw_inp]
+    print(part_one(proc))
+    print(part_two(proc))
---
+++ python_2017/aoc/day9/main.py
@@ -0,0 +1,67 @@
+import os
+from pathlib import Path
+from aoc.helpers import DATA_DIR, parse_input
+
+data = DATA_DIR / "day9.txt"
+
+def remove_cancellations(s):
+    ret = ''
+    total_len = len(s)
+    idx = 0
+    while idx < total_len:
+        if s[idx] != "!":
+            ret = ret + s[idx]
+            idx += 1
+        else:
+            idx += 2
+    return ret
+
+def remove_garbage(s):
+    ret = ''
+    total_len = len(s)
+    idx = 0
+    garbage_count = 0
+    while idx < total_len:
+        if s[idx] != "<":
+            ret = ret + s[idx]
+            idx += 1
+        else:
+            new_idx = idx + 1
+            while s[new_idx] != ">":
+                new_idx += 1
+                garbage_count += 1
+            idx = new_idx + 1
+    return ret, garbage_count
+
+def remove_commas(s):
+    ret = s.replace(",", "")
+    return ret
+
+def calc_score(s):
+    total = 0
+    tally = 0
+    for char in s:
+        if char == "{":
+            tally += 1
+        elif char == "}":
+            total += tally
+            tally -= 1
+    return total
+
+def part_one(s):
+    no_cancels = remove_cancellations(s)
+    no_garbage, _ = remove_garbage(no_cancels)
+    no_comma = remove_commas(no_garbage)
+    final_score = calc_score(no_comma)
+    return final_score
+
+def part_two(s):
+    no_cancels = remove_cancellations(s)
+    _, garbage_count = remove_garbage(no_cancels)
+    return garbage_count
+
+if __name__ == "__main__":
+    raw = parse_input(data)
+    raw = raw[0].strip()
+    print(f"The total score of all groups in the input is {part_one(raw)}")
+    print(f"The total count of garbage characters is {part_two(raw)}")
