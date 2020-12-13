f02e456f5b52eb57cd2058aedc8b9f3b6d14db4e
Matthew Rose <matthew.rose@maxkelsen.com>
2020-12-13 18:25:46 +1000

    finished day1

--- 2020/1/main.go
+++ 2020/1/main.go
@@ -9,6 +9,8 @@ import (
     "sort"
 )
 
+const total = 2020
+
 func parse(location string) []int {
     f, err := os.Open(location)
     if err != nil {
@@ -27,16 +29,45 @@ func parse(location string) []int {
     return out
 }
 
-func solve(inp []int) (int, int) {
+func solvep1(inp []int) (int, int) {
     p1, p2 := 0, len(inp) - 1
-    fmt.Println(p1, p2)
+    var current_sum int
+    for (current_sum != total) {
+        current_sum = inp[p1] + inp[p2]
+        if (current_sum < total) {
+            p1 += 1
+        } else if (current_sum > total) {
+            p2 -= 1
+        }
+    }
     return inp[p1], inp[p2]
+}
+
+func solvep2(inp []int) (int, int, int) {
+    p1, p2, p3 := 0, 1, len(inp) - 1
+    var current_sum int
+    flag := true
+    for (current_sum != total) {
+        current_sum = inp[p1] + inp[p2] + inp[p3]
+        if (current_sum < total) {
+            if (flag) {
+                p2 += 1
+            } else {
+                p1 += 1
+            }
+            flag = !flag
+        } else if (current_sum > total) {
+            p3 -= 1
+        }
+    }
+    return inp[p1], inp[p2], inp[p3]
 }
 
 func main() {
     entries := parse("1/input.txt")
     sort.Ints(entries)
-    fmt.Println(entries)
-    e1, e2 := solve(entries)
-    fmt.Printf("The final product is: %d\n", e1*e2)
+    e1, e2 := solvep1(entries)
+    fmt.Printf("The final product for part 1 is: %d\n", e1*e2)
+    f1, f2, f3 := solvep2(entries)
+    fmt.Printf("The final product for part 2 is: %d\n", f1*f2*f3)
 }
