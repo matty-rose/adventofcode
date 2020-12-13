7e063ed88a22ca8ba60e894b175cc0009942fd0e
Matthew Rose <matthew.rose@maxkelsen.com>
2020-12-13 16:19:23 +1000

    Add day1 skeleton

--- 2020/1/main.go
+++ 2020/1/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "log"
+    "bufio"
+)
+
+func parse(location string) {
+    f, err := os.Open(location)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+    scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+}
+
+func sort() {
+}
+
+func solve() {
+}
 
 func main() {
-    fmt.Println("Hello World")
+    raw_input = parse("1/input.txt")
+    sorted_list = sort(raw_input)
+    e1, e2 = solve(sorted_list)
+    fmt.Printf("The final product is: %d\n", e1*e2)
 }
