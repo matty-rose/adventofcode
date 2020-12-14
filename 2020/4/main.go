899e64ca6a1a59e9021e1ab6ed783b5986fb9a21
Matthew Rose <matthew.rose@maxkelsen.com>
2020-12-14 19:51:18 +1000

    uncapitalize passport type

--- 2020/4/main.go
+++ 2020/4/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	mapstructure "github.com/mitchellh/mapstructure"
 	"log"
 	"os"
-	mapstructure "github.com/mitchellh/mapstructure"
 	"strings"
 	// "encoding/json"
 )
 
-type Passport struct {
-    byr string
-    iyr string
-    eyr string
-    hgt string
-    hcl string
-    ecl string
-    pid string
-    cid *string
+type passport struct {
+	byr string
+	iyr string
+	eyr string
+	hgt string
+	hcl string
+	ecl string
+	pid string
+	cid *string
 }
 
 func rawParse(location string) []string {
@@ -36,49 +36,49 @@ func rawParse(location string) []string {
 }
 
 func groupPassports(lines []string) []string {
-    var tempString string
-    var out []string
-    for _, l := range lines {
-        if len(l) == 0 {
-            out = append(out, tempString)
-            tempString = ""
-        }
-        tempString += " " + l
-    }
-    return out
+	var tempString string
+	var out []string
+	for _, l := range lines {
+		if len(l) == 0 {
+			out = append(out, tempString)
+			tempString = ""
+		}
+		tempString += " " + l
+	}
+	return out
 }
 
-func parsePassportString(p string) Passport {
-    split := strings.Split(p, " ")
-    var passport Passport
-    passportMap := make(map[string]interface{})
-    for _, s := range split {
-        ssplit := strings.Split(s, ":")
-        if len(ssplit) > 1 {
-            passportMap[ssplit[0]] = ssplit[1]
-        }
-    }
-    fmt.Println(passportMap)
-    err := mapstructure.Decode(passportMap, &passport)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Print(passport)
-    return passport
+func parsePassportString(p string) passport {
+	split := strings.Split(p, " ")
+	var pp passport
+	passportMap := make(map[string]interface{})
+	for _, s := range split {
+		ssplit := strings.Split(s, ":")
+		if len(ssplit) > 1 {
+			passportMap[ssplit[0]] = ssplit[1]
+		}
+	}
+	fmt.Println(passportMap)
+	err := mapstructure.Decode(passportMap, &pp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pp)
+	return pp
 }
 
-func parsePassports(passports []string) []Passport {
-    var out []Passport
-    for _, p := range passports {
-        out = append(out, parsePassportString(p))
-    }
-    return out
+func parsePassports(passports []string) []passport {
+	var out []passport
+	for _, p := range passports {
+		out = append(out, parsePassportString(p))
+	}
+	return out
 }
 
 func main() {
-    raw := rawParse("4/input.txt")
-    passportStrings := groupPassports(raw)
-    // parsePassports(passportStrings)
-    passports := parsePassports(passportStrings)
-    fmt.Println(passports)
+	raw := rawParse("4/input.txt")
+	passportStrings := groupPassports(raw)
+	parsePassports(passportStrings)
+	// passports := parsePassports(passportStrings)
+	// fmt.Println(passports)
 }
