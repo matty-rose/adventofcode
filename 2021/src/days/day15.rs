9e68f4f25a750c4c11c1118500d3cd45a95f3189
Matthew Rose <matthewrose153@gmail.com>
2021-12-23 21:37:35 +1000

    feat: 15 parse and get neighbours
    Signed-off-by: Matthew Rose <matthewrose153@gmail.com>

--- 2021/src/days/day15.rs
+++ 2021/src/days/day15.rs
@@ -1,10 +1,57 @@
 use crate::registry::{DayCommand, Function};
 use crate::utils;
+use std::str::FromStr;
+use std::string::ParseError;
+
+#[derive(Debug, Copy, Clone)]
+struct Node {
+    x: usize,
+    y: usize,
+    entry_cost: usize,
+}
+
+#[derive(Debug)]
+struct Cavern {
+    nodes: Vec<Node>,
+}
+
+impl FromStr for Cavern {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut nodes = Vec::new();
+        for (y, line) in s.lines().enumerate() {
+            for (x, cost) in line.chars().enumerate() {
+                nodes.push(Node {
+                    x,
+                    y,
+                    entry_cost: cost.to_digit(10).unwrap() as usize,
+                })
+            }
+        }
+
+        Ok(Cavern { nodes })
+    }
+}
+
+impl Cavern {
+    fn neighbours(&self, x: usize, y: usize) -> Vec<Node> {
+        self.nodes
+            .iter()
+            .copied()
+            .filter(|n| {
+                (n.x == x && (n.y == y.wrapping_sub(1) || n.y == y + 1))
+                    || (n.y == y && (n.x == x.wrapping_sub(1) || n.x == x + 1))
+            })
+            .collect()
+    }
+}
 
 // Just implement dijkstra - node neighbours are euclidean neighbours, "distances" are the values
 // of entering a node
-fn part1(lines: Vec<String>) {
-    unimplemented!()
+fn part1(cavern: Cavern) {
+    println!("{:?}", cavern);
+    println!("neighbours {:?}", cavern.neighbours(0, 0));
 }
 
 fn part2(lines: Vec<String>) {
@@ -14,8 +61,9 @@ fn part2(lines: Vec<String>) {
 fn main(part: &str, file: Option<&str>) -> Option<()> {
     let filename = file.expect("expected a file for this problem");
     let lines: Vec<String> = utils::read_lines(filename).expect("could not load lines");
+    let cavern = Cavern::from_str(&lines.join("\n")).unwrap();
     match part {
-        "1" => part1(lines),
+        "1" => part1(cavern),
         "2" => part2(lines),
         _ => (),
     }
